d95d843449459096809790aa0078c9b272b09820
Tri M. Nguyen <mail@trimn.net>
2013-03-16 02:27:33 +0100

    Minor cleanup in urls.py

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,9 +16,6 @@ urlpatterns = patterns('',
     (r'^api/', include(game_resource.urls)),
     (r'^api/', include(term_resource.urls)),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
