c8cf4dba8793e53f58ec605c82c5975b1c2a6327
Tri M. Nguyen <mail@trimn.net>
2013-03-16 01:43:16 +0100

    Add the service Resources

---
+++ server/service/api.py
@@ -0,0 +1,25 @@
+from tastypie import fields
+from tastypie.resources import ModelResource
+from service.models import Game, Term, Company, GameRel
+
+class TermResource(ModelResource):
+
+    class Meta:
+        queryset = Term.objects.all();
+        resource_name = 'term'
+        include_resource_uri = False
+
+
+class GameRelResource(ModelResource):
+    term = fields.ToOneField(TermResource, 'term', full=True)
+    class Meta:
+        queryset = GameRel.objects.all()
+
+class GameResource(ModelResource):
+    terms = fields.ToManyField(GameRelResource, 'gamerel_set', related_name='term', full=True)
+    class Meta:
+        queryset = Game.objects.all()
+        resource_name = 'game'
+        include_resource_uri = False
+
+
