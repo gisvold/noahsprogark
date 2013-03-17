a92faab5467c1e8b7f9d420d589ce7e60f54a4c7
Aleks Gisvold <gisvold@gmail.com>
2013-03-17 16:40:35 +0100

    add shared preferences

--- bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/SettingsActivity.java
+++ bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/SettingsActivity.java
@@ -1,16 +1,37 @@
 package no.ntnu.noahsprogark.bedpresbingo;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity{
+	
+	private AppPreferences _appPrefs;
+	
+	EditText playerNameField;
+	Button OKButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		_appPrefs = new AppPreferences(getApplicationContext());
 		setContentView(R.layout.activity_settings);
+		initiateLayout();
+		playerNameField.setText(_appPrefs.getPlayerName());
+		findViewById(R.id.OKButton).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				_appPrefs.savePlayerName(playerNameField.getText().toString());
+			}
+		});
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,4 +40,10 @@ public class SettingsActivity extends Activity {
 		return true;
 	}
 
+
+	private void initiateLayout() {
+		OKButton = (Button)findViewById(R.id.OKButton);
+		playerNameField = (EditText)findViewById(R.id.PlayerNameField);
+	}
+	
 }
