162628fbbf4813872566db2da308bb3eb813000e
Henning M. Wold <henningwold@gmail.com>
2013-04-22 16:19:28 +0200

    Added a toast for whenever you get a bingo

--- bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/BingoType.java
+++ bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/BingoType.java
@@ -1,5 +1,16 @@
 package no.ntnu.noahsprogark.bedpresbingo;
 
 public enum BingoType {
-	NONE, SINGLE, DOUBLE, TRIPLE, MEGA, GOLDEN_SINGLE, GOLDEN_DOUBLE, GOLDEN_TRIPLE, GOLDEN_MEGA
+	NONE(0), SINGLE(1), GOLDEN_SINGLE(2), DOUBLE(3), GOLDEN_DOUBLE(4), TRIPLE(5), GOLDEN_TRIPLE(
+			6), MEGA(7), GOLDEN_MEGA(8);
+
+	private int value;
+
+	private BingoType(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
 }
--- bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/GameActivity.java
+++ bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/GameActivity.java
@@ -3,10 +3,14 @@ package no.ntnu.noahsprogark.bedpresbingo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class GameActivity extends Activity implements
 		BingoView.OnCellTouchListener {
 	private BingoView view = null;
+
+	private BingoType lastBingo = BingoType.NONE;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,6 +35,15 @@ public class GameActivity extends Activity implements
 	@Override
 	public void onTouch(BingoCell c) {
 		c.toggleSelected();
+		BingoType currentBingo = view.hasBingo();
+		if (currentBingo.getValue() > lastBingo.getValue()) {
+			Toast t = Toast.makeText(getApplicationContext(),
+					"Congratulations you got a " + currentBingo.name()
+							+ " bingo!", Toast.LENGTH_SHORT);
+			t.setGravity(Gravity.CENTER, 0, 0);
+			t.show();
+		}
+		lastBingo = currentBingo;
 		Log.d("DERP", view.hasBingo().name());
 		view.invalidate();
 	}
