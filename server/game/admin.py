6f5d51e588b2eecd336e211c1492c5e8dc9fa4cc
Tri M. Nguyen <mail@trimn.net>
2013-04-23 16:08:05 +0200

    Clean game app admin.py to only contain its own admin panel stuff.

--- server/game/admin.py
+++ server/game/admin.py
@@ -1,20 +1,7 @@
 from django.contrib import admin
 
 from game.models import Game
-from term.models import Term
-from company.models import Company
-from board.models import Board
-from player.models import Player
-
-class BoardAdmin(admin.ModelAdmin):
-    pass
-
-class PlayerAdmin(admin.ModelAdmin):
-    pass
-
-class TermsInline(admin.TabularInline):
-    model = Game.terms.through
-
+from term.admin import TermsInline
 
 class GameAdmin(admin.ModelAdmin):
     inlines = [
@@ -22,17 +9,4 @@ class GameAdmin(admin.ModelAdmin):
         ]
     exclude = ('terms','create_date',)
 
-class TermAdmin(admin.ModelAdmin):
-    inlines = [
-        TermsInline,
-        ]
-
-
-class CompanyAdmin(admin.ModelAdmin):
-    pass
-
 admin.site.register(Game, GameAdmin)
-admin.site.register(Term, TermAdmin)
-admin.site.register(Company, CompanyAdmin)
-admin.site.register(Board, BoardAdmin)
-admin.site.register(Player, PlayerAdmin)
