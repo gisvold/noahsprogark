e22f3ac5d0978d023ec9eab261902445fec21852
Henning M. Wold <henningwold@gmail.com>
2013-04-24 21:42:12 +0200

    Updated server and database to contain point fields, and leader, for bingos

--- server/game/api.py
+++ server/game/api.py
@@ -1,12 +1,16 @@
 from tastypie import fields
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 
 from game.models import Game
 
 class GameResource(ModelResource):
     company = fields.ForeignKey('company.api.CompanyResource', 'company')
     golden_word = fields.ForeignKey('term.api.TermResource', 'golden_word', full=True)
+    bingo_leader = fields.ForeignKey('player.api.PlayerResource', 'bingo_leader', full=True, null=True)
+
     class Meta:
         queryset = Game.objects.all()
         resource_name = 'game'
+        authorization = Authorization()
         ordering = [ 'id' ]
--- server/game/models.py
+++ server/game/models.py
@@ -4,6 +4,7 @@ from tastypie.utils.timezone import now
 
 from company.models import Company
 from term.models import Term
+from player.models import Player
 
 class Game(models.Model):
     company = models.ForeignKey(Company)
@@ -12,6 +13,8 @@ class Game(models.Model):
     board_size = models.IntegerField()
     create_date = models.DateTimeField(default=now)
     terms = models.ManyToManyField(Term, related_name='t+')
+    bingo_value = models.IntegerField(default=0)
+    bingo_leader = models.ForeignKey(Player, null=True, blank=True)
 
     def __unicode__(self):
         return self.title
