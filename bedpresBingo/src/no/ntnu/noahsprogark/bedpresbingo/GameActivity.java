a0bbded4f88306d8e04f6cd18f274a176ecd5e04
Henning M. Wold <henningwold@gmail.com>
2013-04-25 14:42:50 +0200

    Added JavaDoc to GameActivity

--- bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/GameActivity.java
+++ bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/GameActivity.java
@@ -19,6 +19,10 @@ public class GameActivity extends Activity implements
 	private BingoType lastBingo = BingoType.NONE;
 	private ServerCommunication s;
 
+	/**
+	 * Sets up the BingoBoard by getting words from the server based on the
+	 * hostName and playerName stored in the settings.
+	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game_screen);
@@ -62,11 +66,22 @@ public class GameActivity extends Activity implements
 		}
 	}
 
+	/**
+	 * Calls the BingoView.resetBingoLeader() method whenever the application is
+	 * brought back from the background.
+	 */
 	public void onResume() {
 		super.onResume();
 		view.resetBingoLeader();
 	}
 
+	/**
+	 * Tries to call the updateBingo method in {@link ServerCommunication} when
+	 * the local bingo is higher than the one on the server.
+	 *
+	 * @param bt
+	 *            The BingoType to update to.
+	 */
 	public void updateBingo(BingoType bt) {
 		try {
 			s.updateBingo(bt);
@@ -82,7 +97,11 @@ public class GameActivity extends Activity implements
 		}
 	}
 
-	@Override
+	/**
+	 * Displays a toast message if the user's bingo "level" has increased since
+	 * the last time. Also toggles the touched {@link BingoCell} and redraws the
+	 * view.
+	 */
 	public void onTouch(BingoCell c) {
 		c.toggleSelected();
 		BingoType currentBingo = view.hasBingo();
@@ -94,6 +113,12 @@ public class GameActivity extends Activity implements
 		view.invalidate();
 	}
 
+	/**
+	 * Utility method to display a toast message on the game screen.
+	 *
+	 * @param msg
+	 *            The message to display
+	 */
 	public void displayToast(final String msg) {
 		runOnUiThread(new Runnable() {
 			public void run() {
