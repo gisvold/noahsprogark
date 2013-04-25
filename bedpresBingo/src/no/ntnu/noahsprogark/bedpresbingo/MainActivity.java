884925e8e0105ac28118115205f3982db99e8526
Henning M. Wold <henningwold@gmail.com>
2013-04-25 14:46:31 +0200

    Removed menuthingie from MainActivity

--- bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/MainActivity.java
+++ bedpresBingo/src/no/ntnu/noahsprogark/bedpresbingo/MainActivity.java
@@ -3,7 +3,6 @@ package no.ntnu.noahsprogark.bedpresbingo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -28,12 +27,4 @@ public class MainActivity extends Activity {
 			}
 		});
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
 }
